eeebbefd35cf0413ff7b35593e4ea8eaddd61d5a
gmgb <gabriellegayno@gmail.com>
2024-09-05 17:51:44 -0300

    upload dos arquivos

---
+++ dados.js
@@ -0,0 +1,29 @@
+let dados = [
+  "Biblioteca da Meia Noite",
+  "O Sol é Para Todos",
+  "Princess Mononoke: The First Story"
+]
+
+let livros = [
+  {
+    titulo: "Biblioteca da Meia Noite",
+    sinopse: "Uma história épica e visualmente deslumbrante, Princess Mononoke: The First Story de Hayao Miyazaki nos transporta para um mundo onde humanos e espíritos da floresta lutam por sobrevivência. A princesa Mononoke, uma jovem criada por lobos, se torna o centro de um conflito entre as duas forças, desafiando os conceitos de bem e mal.",
+    genero: "Romance",
+    autor: "Matt Haig",
+    ano: "2020",
+  },
+  {
+    titulo: "O Sol é Para Todos",
+    sinopse: "Uma história sobre raça e classe, inocência e justiça, hipocrisia e heroísmo, tradição e transformação, O sol é para todos permanece tão importante hoje quanto foi em sua primeira edição, em 1960, durante os anos turbulentos da luta pelos direitos civis dos negros nos Estados Unidos.",
+    genero: "Romance",
+    autor: "Harper Lee",
+    ano: "2006",
+  },
+  {
+    titulo: "Princess Mononoke: The First Story",
+    sinopse: "Uma história épica e visualmente deslumbrante, Princess Mononoke: The First Story, de Hayao Miyazaki nos transporta para um mundo onde humanos e espíritos da floresta lutam por sobrevivência. A princesa Mononoke, uma jovem criada por lobos, se torna o centro de um conflito entre as duas forças, desafiando os conceitos de bem e mal.",
+    genero: "Fantasia",
+    autor: "Hayao Miyazaki",
+    ano: "1993",
+  }
+];
