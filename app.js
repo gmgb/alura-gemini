43069352b923605bb0d265aec03edf6e68fec5d2
gmgb <gabriellegayno@gmail.com>
2024-09-06 15:07:59 -0300

    Aula 5

--- app.js
+++ app.js
@@ -1,1 +1,49 @@
-console.log(dados, livros)
+function pesquisar() {
+  // Obtém a seção onde os resultados serão exibidos
+  let section = document.getElementById("resultados-pesquisa");
+
+  let campoPesquisa = document.getElementById("campo-pesquisa").value
+
+  // se campoPesquisa for uma string sem nada
+  if (!campoPesquisa) {
+    section.innerHTML = "<p class='nenhum-resultado'>É preciso exigir de cada um o que cada um pode dar.</p>";
+    return
+  }
+
+  campoPesquisa = campoPesquisa.toLowerCase()
+
+  // Inicializa uma string vazia para armazenar os resultados
+  let resultado = "";
+  let titulo = "";
+  let sinopse = "";
+  let tags = "";
+
+  // Itera sobre cada livro na lista de livros
+  for (let dado of livros) {
+    titulo = dado.titulo.toLowerCase()
+    sinopse = dado.sinopse.toLowerCase()
+    tags = dado.tags.toLowerCase()
+    // se titulo includes campoPesquisa
+    if (titulo.includes(campoPesquisa) || sinopse.includes(campoPesquisa) || tags.includes(campoPesquisa)) {
+      // Cria um novo elemento div para cada livro, formatando os dados
+      resultado += `
+      <div class="item-resultado">
+        <h2>
+         <a href="#" target="_blank">${dado.titulo}</a>
+        </h2>
+        <p class="descricao-meta">${dado.sinopse}</p>
+        <p class="autor">${dado.autor}</p>
+        <p class="ano">${dado.ano}</p>
+        <a href=${dado.link} target="_blank">Mais informações</a>
+      </div>
+    `;
+    }
+  }
+
+  if (!resultado) {
+    resultado = "<p class='nenhum-resultado'>Só se vê bem com o coração, o essencial é invisível aos olhos.</p>";
+  }
+
+  // Atualiza o conteúdo da seção com os resultados formatados
+  section.innerHTML = resultado;
+}
--- dados.js
+++ dados.js
@@ -7,10 +7,12 @@ let dados = [
 let livros = [
   {
     titulo: "Biblioteca da Meia Noite",
-    sinopse: "Uma história épica e visualmente deslumbrante, Princess Mononoke: The First Story de Hayao Miyazaki nos transporta para um mundo onde humanos e espíritos da floresta lutam por sobrevivência. A princesa Mononoke, uma jovem criada por lobos, se torna o centro de um conflito entre as duas forças, desafiando os conceitos de bem e mal.",
+    sinopse: "A Biblioteca da Meia-Noite é um romance incrível que fala dos infinitos rumos que a vida pode tomar e da busca incessante pelo rumo certo. Nesta edição limitada, você ganha outro exemplar do livro embalado para presente.",
     genero: "Romance",
     autor: "Matt Haig",
     ano: "2020",
+    link: "https://www.google.com.br/books/edition/A_Biblioteca_da_Meia_Noite/ud06EAAAQBAJ?hl=pt-BR&gbpv=0",
+    tags: "tempo"
   },
   {
     titulo: "O Sol é Para Todos",
@@ -18,6 +20,8 @@ let livros = [
     genero: "Romance",
     autor: "Harper Lee",
     ano: "2006",
+    link: "https://www.google.com.br/books/edition/O_Sol_É_Para_Todos/g09kCAAAQBAJ?hl=pt-BR&gbpv=0",
+    tags: " racismo justiça social drama jurídico segregação racial direitos civis Harper Lee clássico da literatura americana livro sobre preconceito história real adaptada"
   },
   {
     titulo: "Princess Mononoke: The First Story",
@@ -25,5 +29,16 @@ let livros = [
     genero: "Fantasia",
     autor: "Hayao Miyazaki",
     ano: "1993",
+    link: "https://www.amazon.com.br/Princess-Mononoke-First-Hayao-Miyazaki/dp/1421575868",
+    tags: " animação japonesa Studio Ghibli ecologia mitologia japonesa princesa mononoke filme sobre natureza animação sobre animais conflito homem x natureza fantasia épica"
+  },
+  {
+    titulo: "O Pequeno Príncipe (Le Petit Prince)",
+    sinopse: "Uma história clássica e atemporal sobre amizade, amor e a importância de olhar para o mundo com os olhos da criança. Um piloto, cujo avião quebra no deserto do Saara, encontra um menino misterioso vindo de um asteroide distante. Juntos, embarcam em uma jornada emocionante e repleta de reflexões sobre a vida, o universo e tudo mais.",
+    genero: "Ficção, Literatura Infantil, Conto Filosófico",
+    autor: "Antoine de Saint-Exupéry",
+    ano: "1943",
+    link: "https://www.google.com.br/books/edition/O_pequeno_pr%C3%ADncipe/MSw0EAAAQBAJ?hl=pt-BR&gbpv=0",
+    tags: "clássico da literatura, literatura infantil, filosofia, amizade, amor, contos, Antoine de Saint-Exupéry, fábula, reflexão, existência humana, planetas, rosa, cordeiro, aviador",
   }
 ];
